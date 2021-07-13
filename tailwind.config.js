a218b1e23b350b11c6547d55a6f059095f90ce8a
kcabo <37576764+kcabo@users.noreply.github.com>
2021-07-13 14:13:57 +0900

    🔧 フォント設定変更

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,19 @@
 module.exports = {
   mode: 'jit',
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: [
+          'Avenir',
+          '"Helvetica Neue"',
+          'Helvetica',
+          '"Hiragino Sans"',
+          'ヒラギノ角ゴシック',
+          'Arial',
+          'sans-serif',
+        ],
+      },
+    },
+  },
 };

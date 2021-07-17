60f97ff9bd33022c903bf8b2ff0d305d46ecb89b
kcabo <37576764+kcabo@users.noreply.github.com>
2021-07-17 10:16:29 +0900

    💄 レスポンシブ要素幅の調整

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ import Footer from './components/Footer';
 
 const Main: React.VFC = () => {
   return (
-    <div className='px-10 py-20 flex flex-col md:flex-row items-center justify-center'>
-      <div className='lg:max-w-sm md:w-1/2 md:pr-16 md:text-left mb-16 md:mb-0 text-center'>
+    <div className='px-5 md:px-10 py-20 flex flex-col md:flex-row justify-center md:gap-10'>
+      <div className='lg:max-w-lg w-full md:w-1/2 md:text-left mb-16 md:mb-0 text-center'>
         <About />
         <Preview />
       </div>
@@ -20,7 +20,7 @@ const Main: React.VFC = () => {
 
 const App: React.VFC = () => {
   return (
-    <div className='bg-img bg-cover min-h-screen w-screen text-white'>
+    <div className='bg-img bg-cover bg-center lg:bg-left min-h-screen w-screen text-white'>
       <Main />
       <Footer />
     </div>

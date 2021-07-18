6c7036dc5a37cd9147210a5907ba75839cf5767b
kcabo <37576764+kcabo@users.noreply.github.com>
2021-07-19 00:22:24 +0900

    💄 githubアイコンを追加, fix #4

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import logo from '../images/logo-wh-pk-inner.svg';
+import github from '../images/github.svg';
 
 const Footer: React.VFC = () => {
   return (
-    <div className='w-screen bg-gray-800 bg-opacity-50 flex justify-between fixed bottom-0 px-8 py-5'>
-      <p className='text-2xl'>Sunae</p>
-      <img src={logo} className='w-8' />
+    <div className='w-screen bg-gray-800 bg-opacity-50 flex justify-between fixed bottom-0 px-8 py-3'>
+      <p className='text-xl w-6'>Sunae</p>
+      <a href='https://github.com/kcabo/Sunae'>
+        <img src={github} className='w-8' />
+      </a>
+      <img src={logo} className='w-6' />
     </div>
   );
 };

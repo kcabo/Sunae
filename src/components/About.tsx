facf2d5cf01f4439ad1c08c74ddd9aaa239a82ad
kcabo <37576764+kcabo@users.noreply.github.com>
2021-07-18 18:24:57 +0900

    💄 説明文字列を縁取り, fix #8

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,7 +4,7 @@ const About: React.VFC = () => {
   return (
     <div className='md:text-left'>
       <h1 className='text-4xl md:text-6xl mb-4 font-bold'>砂絵</h1>
-      <p className='mb-6 leading-relaxed text-lg drop-shadow-lg'>
+      <p className='mb-6 leading-relaxed text-lg md:text-outline'>
         Pick your favorite color to customize the editor.
       </p>
     </div>

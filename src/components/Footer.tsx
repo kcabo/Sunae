b34fd10fccf1259aa78fd907ab669f2ff3001a31
kcabo <37576764+kcabo@users.noreply.github.com>
2021-07-19 13:16:24 +0900

    💄 フッターデザインの修正

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,12 +4,12 @@ import github from '../images/github.svg';
 
 const Footer: React.VFC = () => {
   return (
-    <div className='w-screen bg-gray-800 bg-opacity-50 flex justify-between fixed bottom-0 px-8 py-3'>
-      <p className='text-xl w-6'>Sunae</p>
+    <div className='w-screen bg-gray-800 bg-opacity-50 flex justify-between fixed bottom-0 px-8 py-2'>
+      <p className='text-xl w-5 '>Sunae</p>
       <a href='https://github.com/kcabo/Sunae'>
         <img src={github} className='w-8' />
       </a>
-      <img src={logo} className='w-6' />
+      <img src={logo} className='w-5' />
     </div>
   );
 };

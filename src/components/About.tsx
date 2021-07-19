ee955d42a5945ab660e68f17b5f2dc87d0124a4c
kcabo <37576764+kcabo@users.noreply.github.com>
2021-07-19 15:05:35 +0900

    💄 ページの説明を追加, fix #7

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,8 +4,15 @@ const About: React.VFC = () => {
   return (
     <div className='md:text-left'>
       <h1 className='text-4xl md:text-6xl mb-4 font-bold'>砂絵</h1>
-      <p className='mb-6 leading-relaxed text-lg md:text-outline'>
-        Pick your favorite color to customize the editor.
+      <p className='mb-1 leading-relaxed md:text-outline'>
+        A handy and simple WYSIWYG text editor for your browser.{' '}
+        <br className='hidden lg:block' />
+        Build your own editor from the{' '}
+        <span className='hidden md:inline'>right</span> pane{' '}
+        <span className='inline md:hidden'>below</span>.
+      </p>
+      <p className='mb-6 text-gray-400'>
+        <a href='https://github.com/kcabo/Sunae#readme'>Read Document →</a>
       </p>
     </div>
   );
